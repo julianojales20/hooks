73e8c8b82defeaccab8181df0e137c94ca3b27f0
Juliano Alonso <juliano_jales20@hotmail.com>
2024-12-28 19:20:17 -0300

    useContext aula 01

---
+++ src/data/DataContext.js
@@ -0,0 +1,10 @@
+import React, { createContext } from "react";
+
+export const data = {
+  number: 123,
+  text: "Context API...."
+};
+
+const DataContext = createContext(null);
+
+export default DataContext;
