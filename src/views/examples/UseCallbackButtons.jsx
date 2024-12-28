380aeb5c763fae5f07f7115d2868a112ad6c50e8
Juliano Alonso <juliano_jales20@hotmail.com>
2024-12-28 18:43:15 -0300

    useCallback

---
+++ src/views/examples/UseCallbackButtons.jsx
@@ -0,0 +1,35 @@
+import React, { memo } from "react";
+
+const UseCallbackButtons = (props) => {
+  console.log("render...");
+  return (
+    <div>
+      <button
+        className="btn"
+        onClick={() => {
+          props.inc(6);
+        }}
+      >
+        +6
+      </button>
+      <button
+        className="btn"
+        onClick={() => {
+          props.inc(12);
+        }}
+      >
+        +12
+      </button>
+      <button
+        className="btn"
+        onClick={() => {
+          props.inc(18);
+        }}
+      >
+        +18
+      </button>
+    </div>
+  );
+};
+
+export default memo(UseCallbackButtons);
