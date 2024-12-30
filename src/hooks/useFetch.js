1fcf409d6827c12672a91babf11fad11dfecaf6f
Juliano Alonso <juliano_jales20@hotmail.com>
2024-12-30 04:39:15 -0300

    Meus hooks personalizados

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const useFech = (url, method = "get") => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(url, { method })
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [url, method]);
+
+  return { data, error, loading };
+};
+
+export default useFech;
